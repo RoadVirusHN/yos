1a5eacb2dfa8e8931fdd2ef57423e4f0198e33c1
junseok yun <roadvirushn@gmail.com>
2023-06-13 22:47:40 +0900

    [Chore] Test for animation

---
+++ yos/src/pages/aboutme/AboutMe.tsx
@@ -0,0 +1,14 @@
+
+import "./AboutMe.scss"
+function AboutMe(){
+
+    return (
+        <div className="container">
+            <div className="first"></div>
+            <div className="second"></div>
+            <div className="last"></div>
+        </div>
+    )
+}
+
+export default AboutMe
